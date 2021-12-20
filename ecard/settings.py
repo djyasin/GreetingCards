fb02ee7e6b4e3b4b957ae99515a992807efd8987
Jason Judkins <34417573+jcjudkins@users.noreply.github.com>
2021-12-20 17:34:45 -0500

    starting to implement cors

--- ecard/settings.py
+++ ecard/settings.py
@@ -52,9 +52,11 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'app',
+    
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'ecard.urls'
