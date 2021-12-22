d2b240d683dca204f8ad8eb076020a3a8487771a
Jason Judkins <34417573+jcjudkins@users.noreply.github.com>
2021-12-22 17:23:44 -0500

    changing local host to try and clear a 400 server error

--- ecard/settings.py
+++ ecard/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://greeterycards.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
